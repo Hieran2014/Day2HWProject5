5f5621bcc89ee7f9020ecf7f67abb35b46e65950
GYTumlisan <gymoa2011@gmail.com>
2023-04-08 12:08:23 -0700

    added more info area() and volume()

--- src/day2git/Day2HW5.java
+++ src/day2git/Day2HW5.java
@@ -8,4 +8,16 @@ public class Day2HW5 {
 		
 		System.out.println("Remote repository");
 	}
+	
+	// Stash
+	public void area(int a, int b) {
+
+		System.out.println((a * b) / 2);
+	}
+
+	// Push
+	public void volume(int a, int b, int c) {
+
+		System.out.println(a * b * c);
+	}
 }
